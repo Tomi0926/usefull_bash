0a5523d096d85a3291343104d8721ae03ae58138
Tomi0926 <50287967+Tomi0926@users.noreply.github.com>
2024-03-02 09:15:40 +0100

    Delete smbConnect.sh

--- smbConnect.sh
+++
@@ -1,6 +0,0 @@
-apt install cifs-utils -y
-mkdir /media/share
-echo "username=tomi password=Kecske123" > /root/.smbcreds
-chmod 400 /root/.smbcreds
-echo "//10.0.0.5/HDD /media/share cifs vers=3.0,credentials=/root/.smbcreds" | sudo tee -a /etc/fstab
-mount -a
