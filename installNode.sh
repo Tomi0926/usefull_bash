706a4ace5ab002cc801b20a82e42fcb16511f036
Tomi0926 <50287967+Tomi0926@users.noreply.github.com>
2024-03-02 09:43:35 +0100

    Fixed bugs

--- installNode.sh
+++ installNode.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-touch ~/.bashrc
-PATH=$PATH:~/bin
-echo "Installing curl..."
-sudo apt install curl -y  # Use sudo for package management
-
-# Download and install nvm (check for existing installation can be added here)
-echo "Downloading and installing nvm..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-# Source .bashrc to update environment
-source ~/.bashrc
-
-# Install Node.js v20.11.1
-echo "Installing Node.js v20.11.1..."
-nvm install v20.11.1
-
-# List available Node.js versions
-nvm list
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
+apt-get install -y nodejs
